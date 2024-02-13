bb6a02af52f760dc7fd391d308385deb85bf52ef
AjrudeenKhan <ajrudeenkhan1999adk@gmail.com>
2024-02-13 19:15:32 +0530

    U

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-// // import express from 'express';
+
 // import dotenv from 'dotenv';
 // import cors from 'cors';
 // import Connection from './database/db.js';
@@ -6,25 +6,22 @@
 // import DefaultDataThree from './default-three.js';
 // import Router from './routes/route.js';
 // import bodyParser from 'body-parser';
-// import express from 'express'
+// import express from 'express';
 
 // const app = express();
 // dotenv.config();
-// app.use(cors());
+//  app.use(cors());
 
 // app.use(bodyParser.json({ extended: true }));
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use('/', Router);
 
-// // New route to serve the link
-//  app.get('/flipkart', (req, res) => {
-//    res.redirect('https://vercel.com/ajrudeen-khans-projects/flip-kart-mern-backend/5MZ13ifX8h3Pncm7UTHkFZYRZQmy');
-//  });
-
+// // const PORT = process.env.PORT || 8000;
 // const PORT = 8000;
 
 // const USERNAME = process.env.DB_USERNAME;
 // const PASSWORD = process.env.DB_PASSWORD;
+
 // Connection(USERNAME, PASSWORD);
 
 // app.listen(PORT, () => {
@@ -33,9 +30,8 @@
 //   DefaultDataThree();
 // });
 
-// ............................................... 2 ...................................
 
-// import express from 'express';
+import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import Connection from './database/db.js';
@@ -43,19 +39,15 @@ import DefaultData from './default.js';
 import DefaultDataThree from './default-three.js';
 import Router from './routes/route.js';
 import bodyParser from 'body-parser';
-import express from 'express';
 
 const app = express();
 dotenv.config();
- app.use(cors());
-
+app.use(cors());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', Router);
 
-// const PORT = process.env.PORT || 8000;
-const PORT = 8000;
-
+const PORT = process.env.PORT || 8000;
 const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
 
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,28 +1,50 @@
-import express from 'express';
+// import express from 'express';
 
+// import { userSignUp, userLogIn } from '../controller/user-controller.js';
+// import { getProducts,getProductById} from '../controller/product-controller.js';
+// import{ getProductsThree} from '../controller/product-controller-three.js'
+// import { getProducts2 } from '../controller/product-controller2.js';
+// //import { getProducts3 } from '../controller/product-controller3.js';
+// import { getProductsFive } from '../controller/product-controller-five.js';
+// import { addPaymentGateway } from '../controller/payment-controller.js';
+
+
+// const router = express.Router();
+// router.post('/signup',userSignUp);
+// router.post('/login',userLogIn);
+// router.get('/products',getProducts);
+// router.get('/productThree',getProductsThree);
+// //router.get('/productsFiv',getProductsFive)
+// //router.get('/products2',getProducts2)
+// //router.get('/products3',getProducts3)
+// router.get('/product/:id',getProductById)
+// router.post('/payment',addPaymentGateway);
+// router.get('*',(req,res)=>{
+//     res.send('Hii FlipKart Clone API ........')
+// })
+
+
+
+// export default router;
+
+import express from 'express';
 import { userSignUp, userLogIn } from '../controller/user-controller.js';
-import { getProducts,getProductById} from '../controller/product-controller.js';
-import{ getProductsThree} from '../controller/product-controller-three.js'
-import { getProducts2 } from '../controller/product-controller2.js';
-//import { getProducts3 } from '../controller/product-controller3.js';
-import { getProductsFive } from '../controller/product-controller-five.js';
+import { getProducts, getProductById } from '../controller/product-controller.js';
+import { getProductsThree } from '../controller/product-controller-three.js';
 import { addPaymentGateway } from '../controller/payment-controller.js';
 
-
 const router = express.Router();
-router.post('/signup',userSignUp);
-router.post('/login',userLogIn);
-router.get('/products',getProducts);
-router.get('/productThree',getProductsThree);
-//router.get('/productsFiv',getProductsFive)
-//router.get('/products2',getProducts2)
-//router.get('/products3',getProducts3)
-router.get('/product/:id',getProductById)
-router.post('/payment',addPaymentGateway);
-router.get('*',(req,res)=>{
-    res.send('Hii FlipKart Clone API ........')
-})
 
+router.post('/signup', userSignUp);
+router.post('/login', userLogIn);
+router.get('/products', getProducts);
+router.get('/productThree', getProductsThree);
+router.get('/product/:id', getProductById);
+router.post('/payment', addPaymentGateway);
 
+// Catch-all route for other paths
+router.get('*', (req, res) => {
+  res.send('Hii FlipKart Clone API ........');
+});
 
 export default router;
