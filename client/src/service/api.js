31848224594de9b01cff04e000f8d18c5d5b62ab
AjrudeenKhan <ajrudeenkhan1999adk@gmail.com>
2024-02-13 20:45:28 +0530

    U

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
- const URL = 'http://localhost:8000';
+ //const URL = 'http://localhost:8000';
 
 export const authenticateSignup = async (data)=>{
     try{
-       return await axios.post(`${URL}/signup`,data);
-      // return await axios.post(`https://flip-kart-mern-server.vercel.app/signup`,data);
+     //  return await axios.post(`${URL}/signup`,data);
+       return await axios.post(`https://flip-kart-mern-5mxo.vercel.app/signup`,data);
 
     } catch(error){
         console.log("Error while calling signup api",error.response.data);
@@ -14,7 +14,7 @@ export const authenticateSignup = async (data)=>{
 export const authenticateLogin = async (data)=>{
     
     try{
-       return await axios.post(`${URL}/login`,data);
+       return await axios.post(`https://flip-kart-mern-5mxo.vercel.app/login`,data);
 
     } catch(error){
         console.log("Error while calling login api",error);
@@ -24,7 +24,7 @@ export const authenticateLogin = async (data)=>{
 
 export const payUsingPayment = async (data) =>{
     try{
-        let response = await axios.post(`${URL}/payment`,data);
+        let response = await axios.post(` https://flip-kart-mern-5mxo.vercel.app/payment`,data);
         return response.data;
 
     }catch(error){
