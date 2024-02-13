6362c927f08af89a95925edc7d25119386a3e62f
AjrudeenKhan <ajrudeenkhan1999adk@gmail.com>
2024-02-13 20:58:33 +0530

    Update

--- client/src/redux/actions/productAction.js
+++ client/src/redux/actions/productAction.js
@@ -37,12 +37,12 @@
 import axios from "axios";
 import * as actionTypes from '../constants/productConstant';
 
-//const URL = 'http://localhost:8000';
+const URL = 'http://localhost:8000';
 
 export const getProducts = () => async (dispatch) => {
   try {
-   // const response = await axios.get(`${URL}/products`);
-    const response = await axios.get(`https://flip-kart-mern-5mxo.vercel.app/products`);
+    const response = await axios.get(`${URL}/products`);
+   // const response = await axios.get(`https://flip-kart-mern.vercel.app/products`);
      console.log(response.data)
     // Check if the response status is in the 2xx range to confirm a successful request.
     if (response.status >= 200 && response.status < 300) {
@@ -60,8 +60,8 @@ export const getProducts = () => async (dispatch) => {
 export const getProductDetails=(id) =>async (dispatch) =>{
   try{
     dispatch({type:actionTypes.GET_PRODUCT_DETAILS_REQUEST});
-   // const {data} = await axios.get(`${URL}/product/${id}`)
-    const {data} = await axios.get(`https://flip-kart-mern-5mxo.vercel.app/products/${id}`)
+    const {data} = await axios.get(`${URL}/product/${id}`)
+    //const {data} = await axios.get(`https://flip-kart-mern.vercel.app/products/${id}`)
 
     dispatch({ type: actionTypes.GET_PRODUCT_DETAILS_SUCCESS, payload:data });
 
--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
- //const URL = 'http://localhost:8000';
+ const URL = 'http://localhost:8000';
 
 export const authenticateSignup = async (data)=>{
     try{
-     //  return await axios.post(`${URL}/signup`,data);
-       return await axios.post(`https://flip-kart-mern-5mxo.vercel.app/signup`,data);
+       return await axios.post(`${URL}/signup`,data);
+      // return await axios.post(`https://flip-kart-mern.vercel.app/signup`,data);
 
     } catch(error){
         console.log("Error while calling signup api",error.response.data);
@@ -14,7 +14,7 @@ export const authenticateSignup = async (data)=>{
 export const authenticateLogin = async (data)=>{
     
     try{
-       return await axios.post(`https://flip-kart-mern-5mxo.vercel.app/login`,data);
+       return await axios.post(`${URL}/login`,data);
 
     } catch(error){
         console.log("Error while calling login api",error);
